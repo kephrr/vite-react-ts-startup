ee33b459dfab01f024716d9e2c3e5df8b9e5c6ae
kephrr <kephnze@gmail.com>
2024-12-03 21:52:29 +0000

    Creating user model

---
+++ src/models/user.tsx
@@ -0,0 +1,14 @@
+export type User = {
+    id: number,
+    name: string,
+    pseudo: string,
+    label:string
+}
+
+export type UserDetails = {
+    id: number,
+    name: string,
+    pseudo: string,
+    score: number,
+    label: string
+}
